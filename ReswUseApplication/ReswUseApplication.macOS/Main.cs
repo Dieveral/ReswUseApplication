9d4ce9bf2d50f765ebe99483b1b2983edfe4a2e2
AlexeyGulevich <alexey.gulevich@temsa.by>
2020-06-04 10:49:52 +0300

    Add project files.

---
+++ ReswUseApplication/ReswUseApplication.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace ReswUseApplication.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
